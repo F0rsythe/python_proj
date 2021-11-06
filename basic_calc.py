6be11c245258b55fc9c9ed60a5c1e03f6183ea2e
F0rsythe <85443939+F0rsythe@users.noreply.github.com>
2021-11-06 09:34:24 +0100

    Add files via upload

---
+++ basic_calc.py
@@ -0,0 +1,13 @@
+#python 2.7.15
+
+print "Hello, Dcoder!"
+#print "How many numbers will you be working with"
+no = input("Hoe many numbers will you be working with")
+num1 = int(input("Enter your first number"))
+num2 = int(input("Enter your second number"))
+op = input("Enter your operator")
+
+if( op == '+'):
+  sum = num1 + num2
+elif (op == "*"):
+  sum = 
